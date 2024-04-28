96ba55dd64685259c0930b8bebb101fcf50f3f9d
Alessandro <fimaja.9@gmail.com>
2024-04-28 15:18:50 +0200

    fixes

--- src/app/components/home/home-routing.module.ts
+++ src/app/components/home/home-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { UtenteComponent } from './utente/utente.component';
-import { InformazioniGeneraliComponent } from './informazioni-generali/informazioni-generali.component';
 import { RicercaComponent } from './ricerca/ricerca.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'dettaglio', component: InformazioniGeneraliComponent},
   {path: 'utente', component: UtenteComponent},
   {path: 'ricerca', component: RicercaComponent},
 
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -7,7 +7,6 @@ import { HomeNavComponent } from './home-nav/home-nav.component';
 import { HomeSearchComponent } from './home-search/home-search.component';
 import { UtenteComponent } from './utente/utente.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { InformazioniGeneraliComponent } from './informazioni-generali/informazioni-generali.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { RicercaComponent } from './ricerca/ricerca.component';
@@ -16,7 +15,7 @@ import { BsDatepickerConfig, BsDatepickerModule } from 'ngx-bootstrap/datepicker
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @NgModule({
-  declarations: [HomeComponent, HomeNavComponent, HomeSearchComponent, UtenteComponent, InformazioniGeneraliComponent, RicercaComponent],
+  declarations: [HomeComponent, HomeNavComponent, HomeSearchComponent, UtenteComponent, RicercaComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
